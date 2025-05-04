de70df3d0b5b3878176b480cefcf27a303a74fa7
Fraidy <fraidygonzalezreinaldo@gmail.com>
2025-05-04 19:07:51 -0400

    add icons

---
+++ components/Icons/IconsServices/ArrowRightIcon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+
+defineComponent({
+  name: "ArrowRightIcon",
+});
+</script>
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="ml-1 h-4 w-4 transition-transform duration-300 group-hover:translate-x-1"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M14 5l7 7m0 0l-7 7m7-7H3"
+    />
+  </svg>
+</template>
---
+++ components/Icons/IconsServices/VirtualizacionIcon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+
+defineComponent({
+  name: "VirtualizacionIcon",
+});
+</script>
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2z"
+    />
+  </svg>
+</template>
