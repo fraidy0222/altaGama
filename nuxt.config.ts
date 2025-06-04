a14b67013498a69a9bf1b4663ddf9a1aecb6214e
Fraidy <fraidygonzalezreinaldo@gmail.com>
2025-06-04 08:37:55 -0400

    add some config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-
+  ui: {
+    fonts: false,
+  },
+  colorMode: {
+    preference: "dark",
+  },
   app: {
     head: {
       title: "Alta Gama S.R.L",
